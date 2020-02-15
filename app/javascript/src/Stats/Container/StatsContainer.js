facb72724f1b9ad9045b7ea13e8a50dc5d86e0d1
Anthonyhelka <helkaanthony1997@gmail.com>
2020-02-14 22:36:48 -0500

    Testing webpack change.

--- app/javascript/src/Stats/Container/StatsContainer.js
+++ app/javascript/src/Stats/Container/StatsContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from '../../../../assets/stylesheets/_StatsContainer.module.scss';
+import classes from './StatsContainer.module.scss';
 import { connect } from 'react-redux';
 import { handleSort } from '../../Redux/modules/statsContainer.js';
 import Leaderboard from '../Leaderboard/Leaderboard.js';
